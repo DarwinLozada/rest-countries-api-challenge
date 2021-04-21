01077ac6bda87455bc599ec6907a82bb6276ae3e
DarwinLozada <darwinlozadadev@gmail.com>
2021-04-21 18:35:59 -0400

    changed header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
 
   return (
     <div className="relative flex flex-row py-6 px-12 w-full justify-between shadow-md bg-white dark:bg-darkBlue dark:text-white items-center transition-colors duration-300 z-10">
-      <h1 className="font-bold text-md mr-4">Where in the world?</h1>
+      <h1 className="font-bold text-md mr-4">Where in this world?</h1>
       <button
         className="flex flex-row items-center focus:outline-none"
         onClick={handleThemeChange}
